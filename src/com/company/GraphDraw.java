c407cbe90a44a0a2aca25357ea9a1bfba3f48863
akimov <akimov.infotech@gmail.com>
2022-03-01 00:27:03 +0600

    using Drawings

---
+++ src/com/company/GraphDraw.java
@@ -0,0 +1,14 @@
+package com.company;
+
+import edu.princeton.cs.algs4.StdDraw;
+
+import java.awt.*;
+
+public class GraphDraw {
+
+    public static void main(String[] args) {
+        StdDraw.setPenColor(Color.black);
+        StdDraw.setPenRadius(.005);
+        StdDraw.point(0.5, 0.5);
+    }
+}
